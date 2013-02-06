cb0d86ae71a7ccbabe8d385812602a8eb79eeff2
Patrick Arlt <patrick.arlt@gmail.com>
2013-02-06 12:17:49 -0800

    rebuild and update arcgis veiwer test

--- dist/browser/terraformer.js
+++ dist/browser/terraformer.js
@@ -1,4 +1,4 @@
-/*! Terraformer JS - 0.0.1 - 2013-01-28
+/*! Terraformer JS - 0.0.1 - 2013-02-06
 *   https://github.com/geoloqi/Terraformer
 *   Copyright (c) 2013 Environmental Systems Research Institute, Inc.
 *   Licensed MIT */
