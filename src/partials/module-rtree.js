a4672c1b9a1cf2e1e0b985b697cbe40ce02dd35d
Patrick Arlt <patrick.arlt@gmail.com>
2013-09-05 09:25:21 -0700

    remove arcgis parser, remove amd support, remove Terraformer.Deferred

--- src/partials/module-rtree.js
+++ src/partials/module-rtree.js
@@ -2,38 +2,18 @@
 
   // Node.
   if(typeof module === 'object' && typeof module.exports === 'object') {
-    exports = module.exports = factory();
+    exports = module.exports = factory(require("terraformer"));
   }
 
-  // AMD.
-  if(typeof define === 'function' && define.amd) {
-    define(["terraformer/terraformer"],factory);
+  if(typeof root.navigator === "object") {
+    if (!root.Terraformer){
+      throw new Error("Terraformer.RTree requires the core Terraformer library. https://github.com/esri/Terraformer");
+    }
+    root.Terraformer.RTree = factory(root.Terraformer).RTree;
   }
-
-  // Browser Global.
-  if (typeof root.Terraformer === "undefined"){
-    root.Terraformer = {};
-  }
-  root.Terraformer.RTree = factory().RTree;
 
 }(this, function() {
   var exports = { };
-  var Terraformer;
-
-  // Local Reference To Browser Global
-  if(typeof this.navigator === "object") {
-    Terraformer = this.Terraformer;
-  }
-
-  // Setup Node Dependencies
-  if(typeof module === 'object' && typeof module.exports === 'object') {
-    Terraformer = require('terraformer');
-  }
-
-  // Setup AMD Dependencies
-  if(arguments[0] && typeof define === 'function' && define.amd) {
-    Terraformer = arguments[0];
-  }
 
   function isArray(obj) {
     return Object.prototype.toString.call(obj) === '[object Array]';
