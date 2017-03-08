d1047f8b2b53f69572c404fc2d9eba152c8ea686
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2017-03-08 11:32:38 -0800

    Added tslint test to package test
    Modified TypeScript files to pass TSLint test.
    Modified TSLint configuration.

--- index.d.ts
+++ index.d.ts
@@ -5,122 +5,132 @@
  */
 
 export interface Envelope {
-    x: number,
-    y: number,
-    w: number,
-    h: number
+    x: number;
+    y: number;
+    w: number;
+    h: number;
 }
-export type BBox = number[]; //[number, number, number, number]
+export type BBox = number[]; // [number, number, number, number]
 
-export type Coordinate = GeoJSON.Position
+export type Coordinate = GeoJSON.Position;
 export type Coordinates = GeoJSON.Position[];
 
 /**
- * Terraformer Primitives are JavaScript objects that map directly to their GeoJSON couterparts. Converting a GeoJSON object into a Terraformer Primitive will allow you use convenience methods like point.within(polygon).
+ * Terraformer Primitives are JavaScript objects that map directly to their GeoJSON couterparts.
+ * Converting a GeoJSON object into a Terraformer Primitive will allow you use convenience methods like
+ * point.within(polygon).
  *
- * Every Primitive inherits from the Terraformer.Primitive base class, thus all other Primitives share the Terraformer.Primitive methods.
+ * Every Primitive inherits from the Terraformer.Primitive base class, thus all other Primitives share the
+ * Terraformer.Primitive methods.
  *
- * There is a Primitive for every type of GeoJSON object, plus a Circle Primitive which represents a circle as a polygon.
+ * There is a Primitive for every type of GeoJSON object, plus a Circle Primitive which represents a circle as a
+ * polygon.
  */
 export class Primitive<T extends GeoJSON.GeoJsonObject> implements GeoJSON.GeoJsonObject {
+    public type: string;
     /**
-     * You create a new Terraformer.Primitive object by passing it a valid GeoJSON Object. This will return a Terraformer.Primitive with the same type as your GeoJSON object.
+     * You create a new Terraformer.Primitive object by passing it a valid GeoJSON Object. This will return a
+     * Terraformer.Primitive with the same type as your GeoJSON object.
      * @param geojson GeoJSON Primitive
      */
     constructor(geojson: T);
-    type: string;
     /**
      * Converts this GeoJSON object’s coordinates to the web mercator spatial reference.
      */
-    toMercator(): this;
+    public toMercator(): this;
     /**
      * Converts this GeoJSON object’s coordinates to geographic coordinates.
      */
-    toGeographic(): this;
+    public toGeographic(): this;
     /**
      * Returns an object with x, y, w and h suitable for passing to most indexes.
      */
-    envelope(): Envelope;
+    public envelope(): Envelope;
     // /**
     //  * Returns the GeoJSON Bounding Box for this primitive.
     //  */
-    // bbox(): number[]; // Terraformer docs have this function, but conflicts with GeoJSON typescript definitions for optional bbox property.
+    // bbox(): number[]; // Terraformer docs have this function, but conflicts with GeoJSON typescript definitions for
+    //                   // optional bbox property.
     /**
-     * Returns the convex hull of this primitive as a Polygon. Will return null if the convex hull cannot be calculated or a valid Polygon cannot be created.
+     * Returns the convex hull of this primitive as a Polygon. Will return null if the convex hull cannot be calculated
+     * or a valid Polygon cannot be created.
      */
-    convexHull(): GeoJSON.Polygon | null;
+    public convexHull(): GeoJSON.Polygon | null;
     /**
      * Returns true if the passed GeoJSON Geometry object is completely contained inside this primitive.
      * @param geometry The geometry that potentially is inside of this one.
      * @returns Returns true if the passed GeoJSON Geometry object is completely contained inside this primitive.
      */
-    contains(geometry: GeoJSON.GeometryObject): boolean;
+    public contains(geometry: GeoJSON.GeometryObject): boolean;
     /**
      * Returns true if the passed GeoJSON Geometry object is completely within this primitive.
      * @param geometry GeoJSON geometry
      */
-    within(geometry: GeoJSON.GeometryObject): boolean;
+    public within(geometry: GeoJSON.GeometryObject): boolean;
     /**
      * Returns true if the passed GeoJSON Geometry intersects this primitive.
      * @param geometry GeoJSON geometry
      */
-    intersects(geometry: GeoJSON.GeometryObject): boolean;
+    public intersects(geometry: GeoJSON.GeometryObject): boolean;
 }
 
 export class Point extends Primitive<GeoJSON.Point> implements GeoJSON.Point {
+    public type: "Point";
+    public coordinates: GeoJSON.Position;
     constructor(p: GeoJSON.Point);
     constructor(x: number, y: number);
     constructor(xy: GeoJSON.Position);
-    type: "Point";
-    coordinates: GeoJSON.Position;
 }
 
 export class MultiPoint extends Primitive<GeoJSON.MultiPoint> implements GeoJSON.MultiPoint {
+    public type: "MultiPoint";
+    public coordinates: GeoJSON.Position[];
+    public forEach: (point: Point, index: number, coordinates: Coordinates) => null;
     constructor(geojsonMP: GeoJSON.MultiPoint);
     constructor(coordinates: Coordinates);
-    type: "MultiPoint";
-    coordinates: GeoJSON.Position[];
-    forEach: (point: Point, index: number, coordinates: Coordinates) => null;
-    get(index: number): Point;
-    addPoint(coordinate: Coordinate): this;
-    insertPoint(coordinate: Coordinate, index: number): this;
-    removePoint(index: number): this;
-    removePoint(coordinate: Coordinate): this;
+    public get(index: number): Point;
+    public addPoint(coordinate: Coordinate): this;
+    public insertPoint(coordinate: Coordinate, index: number): this;
+    public removePoint(index: number): this;
+    public removePoint(coordinate: Coordinate): this;
 }
 
 export class LineString extends Primitive<GeoJSON.LineString> implements GeoJSON.LineString {
+    public type: "LineString";
+    public coordinates: GeoJSON.Position[];
     constructor(geoJson: GeoJSON.LineString);
     constructor(coordinates: Coordinates);
-    type: "LineString";
-    coordinates: GeoJSON.Position[];
-    addVertex(coordinate: Coordinate): this;
-    insertVertex(coordinate: Coordinate, index: number): this;
-    removeVertex(index: number): this;
+    public addVertex(coordinate: Coordinate): this;
+    public insertVertex(coordinate: Coordinate, index: number): this;
+    public removeVertex(index: number): this;
 }
 
 export class MultiLineString extends Primitive<GeoJSON.MultiLineString> implements GeoJSON.MultiLineString {
+    public type: "MultiLineString";
+    public coordinates: Coordinate[][];
+    public forEach: (linestring: LineString, index: number, coordinates: Coordinates) => null;
     constructor(geoJson: GeoJSON.MultiLineString);
     constructor(coordinates: Coordinates[]);
-    type: "MultiLineString";
-    coordinates: Coordinate[][];
-    forEach: (linestring: LineString, index: number, coordinates: Coordinates) => null;
-    get(index: number): LineString
+    public get(index: number): LineString
 }
 
 export class Polygon extends Primitive<GeoJSON.Polygon> implements GeoJSON.Polygon {
+    public type: "Polygon";
+    public coordinates: Coordinate[][];
     constructor(geoJson: GeoJSON.Polygon);
     constructor(coordinates: Coordinates[])
-    type: "Polygon";
-    coordinates: Coordinate[][];
-    addVertex(coordinate: Coordinate): this;
-    insertVertex(coordinate: Coordinate, index: number): this;
-    removeVertex(index: number): this;
-    close(): this;
-    hasHoles(): boolean;
-    holes(): Polygon[]
+    public addVertex(coordinate: Coordinate): this;
+    public insertVertex(coordinate: Coordinate, index: number): this;
+    public removeVertex(index: number): this;
+    public close(): this;
+    public hasHoles(): boolean;
+    public holes(): Polygon[]
 }
 
 export class MultiPolygon extends Primitive<GeoJSON.MultiPolygon> implements GeoJSON.MultiPolygon {
+    public type: "MultiPolygon";
+    public coordinates: Coordinates[][];
+    public forEach: (polygon: Polygon, index: number, coordinates: Coordinates) => null;
     constructor(geoJson: GeoJSON.Polygon);
     constructor(geoJson: GeoJSON.MultiPolygon);
     // // Valid, according to http://terraformer.io/core/#constructor-6
@@ -130,37 +140,34 @@ export class MultiPolygon extends Primitive<GeoJSON.MultiPolygon> implements Geo
     // });
     constructor(coordinates: Coordinates[]);
     constructor(coordinates: Coordinates[][]);
-    type: "MultiPolygon";
-    coordinates: Coordinates[][];
-    forEach: (polygon: Polygon, index: number, coordinates: Coordinates) => null;
-    get(index: number): Polygon
+    public get(index: number): Polygon
 }
 
 export class Feature<T extends GeoJSON.GeometryObject> implements GeoJSON.Feature<T> {
-    type: "Feature";
-    geometry: T
-    properties: any;
+    public type: "Feature";
+    public geometry: T;
+    public properties: any;
     constructor(geometry: T);
     constructor(geoJson: GeoJSON.Feature<T>);
 }
 
 export class FeatureCollection<T extends GeoJSON.GeometryObject> implements GeoJSON.FeatureCollection<T> {
-    type: "FeatureCollection";
-    features: GeoJSON.Feature<T>[];
+    public type: "FeatureCollection";
+    public features: Array<GeoJSON.Feature<T>>;
+    public forEach: (feature: Feature<T>, index: number, coordinates: Coordinates) => null;
     constructor(geoJson: GeoJSON.FeatureCollection<T>);
-    constructor(features: GeoJSON.Feature<T>[]);
-    forEach: (feature: Feature<T>, index: number, coordinates: Coordinates) => null;
-    get(index: number): Feature<T>
+    constructor(features: Array<GeoJSON.Feature<T>>);
+    public get(index: number): Feature<T>
 }
 
 export class GeometryCollection implements GeoJSON.GeometryCollection {
-    type: "GeometryCollection";
-    geometries: GeoJSON.GeometryObject[];
+    public type: "GeometryCollection";
+    public geometries: GeoJSON.GeometryObject[];
+    public forEach: (geometry: GeoJSON.GeometryObject, index: number, coordinates: Coordinates) => null;
     constructor(geoJson: GeoJSON.GeometryCollection);
     constructor(geoJson: GeoJSON.FeatureCollection<GeoJSON.GeometryObject>);
     constructor(features: GeoJSON.GeometryObject[]);
-    forEach: (geometry: GeoJSON.GeometryObject, index: number, coordinates: Coordinates) => null;
-    get(index: number): Primitive<GeoJSON.GeometryObject>
+    public get(index: number): Primitive<GeoJSON.GeometryObject>
 }
 
 /**
@@ -172,9 +179,18 @@ export class GeometryCollection implements GeoJSON.GeometryCollection {
  * circle.contains(point);
  */
 export class Circle extends Primitive<GeoJSON.Feature<GeoJSON.Polygon>> implements GeoJSON.Feature<GeoJSON.Polygon> {
-    type: "Feature";
-    geometry: GeoJSON.Polygon;
-    properties: any;
+    public type: "Feature";
+    public geometry: GeoJSON.Polygon;
+    public properties: any;
+    public steps: (steps?: number) => number;
+    /**
+     * Returns the radius circle. If the radius parameter is passed the circle will be recalculated witht he new radius before returning.
+     */
+    public radius: (radius?: number) => number;
+    /**
+     * Returns the center of the circle. If the center parameter is passed the circle will be recalculated with the new center before returning.
+     */
+    public center: (center?: Coordinate) => Coordinates;
     /**
      * Terraformer.Circle is created with a center, radius, and steps.
      * @param [center=null] Required A GeoJSON Coordinate in [x,y] format.
@@ -185,19 +201,10 @@ export class Circle extends Primitive<GeoJSON.Feature<GeoJSON.Polygon>> implemen
     /**
      * Recalculates the circle
      */
-    recalculate(): this;
+    public recalculate(): this;
     /**
      * Returns the number of steps to produce the polygon representing the circle. If the steps parameter is passed the circle will be recalculated witht he new step count before returning.
      */
-    steps: (steps?: number) => number;
-    /**
-     * Returns the radius circle. If the radius parameter is passed the circle will be recalculated witht he new radius before returning.
-     */
-    radius: (radius?: number) => number;
-    /**
-     * Returns the center of the circle. If the center parameter is passed the circle will be recalculated with the new center before returning.
-     */
-    center: (center?: Coordinate) => Coordinates;
 }
 
 /**
@@ -211,28 +218,28 @@ export class Tools {
      * Converts this GeoJSON object’s coordinates to the web mercator spatial reference. This is an in-place modification of the passed object.
      * @param geojson GeoJSON object
      */
-    static toMercator(geojson: GeoJSON.GeoJsonObject): GeoJSON.GeoJsonObject;
+    public static toMercator(geojson: GeoJSON.GeoJsonObject): GeoJSON.GeoJsonObject;
     /**
      * Converts this GeoJSON object’s coordinates to geographic coordinates. This is an in-place modification of the passed object.
      * @param geojson GeoJSON object
      */
-    static toGeographic(geojson: GeoJSON.GeoJsonObject): GeoJSON.GeoJsonObject;
+    public static toGeographic(geojson: GeoJSON.GeoJsonObject): GeoJSON.GeoJsonObject;
     /**
      * Runs the passed function against every Coordinate in the geojson object. Your function will be passed a Coordinate and will be expected to return a Coordinate.
      * @param geojson GeoJSON object
      * @param converter Function to convert one coordinate to a different coordinate.
      */
-    static applyConverter(geojson: GeoJSON.GeoJsonObject, converter: (coordinate: Coordinate) => Coordinate): GeoJSON.GeoJsonObject;
+    public static applyConverter(geojson: GeoJSON.GeoJsonObject, converter: (coordinate: Coordinate) => Coordinate): GeoJSON.GeoJsonObject;
     /**
      * Converts the passed Coordinate to web mercator spatial reference.
      * @param coordinate Coordinate
      */
-    static positionToMercator(coordinate: Coordinate): Coordinate;
+    public static positionToMercator(coordinate: Coordinate): Coordinate;
     /**
      * Converts the passed Coordinate to geographic coordinates.
      * @param coordinate Coordinate to convert
      */
-    static positionToGeographic(coordinate: Coordinate): Coordinate;
+    public static positionToGeographic(coordinate: Coordinate): Coordinate;
 
     // Calculations
 
@@ -240,17 +247,17 @@ export class Tools {
      * Returns a GeoJSON bounding box for the passed geoJSON.
      * @param geojson
      */
-    static calculateBounds(geojson: GeoJSON.GeoJsonObject): BBox;
+    public static calculateBounds(geojson: GeoJSON.GeoJsonObject): BBox;
     /**
      * Returns an object with x, y, w, h. Suitable for passing to most indexes.
      * @param geojson
      */
-    static calculateEnvelope(geojson: GeoJSON.GeoJsonObject): Envelope;
+    public static calculateEnvelope(geojson: GeoJSON.GeoJsonObject): Envelope;
     /**
      * Returns an array of coordinates representing the convex hull the the passed geoJSON.
      * @param geojson
      */
-    static convexHull(geojson: Coordinates): Coordinates;
+    public static convexHull(geojson: Coordinates): Coordinates;
 
     // Comparisons
 
@@ -259,19 +266,19 @@ export class Tools {
      * @param coordinates Array of coordinates
      * @param coordinate Coordinate that will be searched for in the array.
      */
-    static coordinatesContainPoint(coordinates: Coordinates[], coordinate: Coordinate): Boolean;
+    public static coordinatesContainPoint(coordinates: Coordinates[], coordinate: Coordinate): Boolean;
     /**
      * Accepts the geometry of a polygon and a coordinate and returns true if the point falls within the polygon.
      * @param polygon
      * @param coordinate
      */
-    static polygonContainsPoint(polygon: GeoJSON.Polygon, coordinate: Coordinate): Boolean;
+    public static polygonContainsPoint(polygon: GeoJSON.Polygon, coordinate: Coordinate): Boolean;
     /**
      * Accepts the geometry of a polygon and a coordinate and returns true if the point falls within the polygon.
      * @param polygon
      * @param coordinate
      */
-    static polygonContainsPoint(polygon: GeoJSON.Position[][], coordinate: Coordinate): Boolean;
+    public static polygonContainsPoint(polygon: GeoJSON.Position[][], coordinate: Coordinate): Boolean;
     /**
      * Accepts two arrays of coordinates and returns true if they cross each other at any point.
      * @param c1
@@ -298,11 +305,11 @@ export class Tools {
      * Terraformer.Tools.polygonContainsPoint(polygonGeometry, pt2);
      * // returns true
      */
-    static arrayIntersectsArray(c1: Coordinates[], c2: Coordinates[]): Boolean;
+    public static arrayIntersectsArray(c1: Coordinates[], c2: Coordinates[]): Boolean;
     /**
      * Accepts two individual coordinate pairs and returns true if the passed coordinate pairs are equal to each other.
      * @param c1
      * @param c2
      */
-    static coordinatesEqual(c1: Coordinate, c2: Coordinate): Boolean;
+    public static coordinatesEqual(c1: Coordinate, c2: Coordinate): Boolean;
 }
--- test.ts
+++ test.ts
@@ -4,7 +4,7 @@ console.assert(typeof Terraformer !== undefined);
 
 let point = new Terraformer.Primitive({
   type: "Point",
-  coordinates: [1, 2],
+  coordinates: [1, 2]
 });
 
 console.assert(point instanceof Terraformer.Point); // -> true
@@ -14,7 +14,7 @@ console.assert(point instanceof Terraformer.Primitive); // -> true
 
 let point1 = new Terraformer.Point({
   type: "Point",
-  coordinates: [1, 2],
+  coordinates: [1, 2]
 });
 
 let point2 = new Terraformer.Point(1, 2);
@@ -23,14 +23,14 @@ let point3 = new Terraformer.Point([1, 2]);
 
 let linestring = new Terraformer.LineString({
   type: "LineString",
-  coordinates: [[1, 2], [2, 1]],
+  coordinates: [[1, 2], [2, 1]]
 });
 
 linestring = new Terraformer.LineString([[1, 2], [2, 1]]);
 
 let multilinestring = new Terraformer.MultiLineString({
   type: "MultiLineString",
-  coordinates: [[[1, 2], [2, 1]]],
+  coordinates: [[[1, 2], [2, 1]]]
 });
 
 multilinestring = new Terraformer.MultiLineString([[[1, 1], [2, 2], [3, 4]], [[0, 1], [0, 2], [0, 3]]]);
@@ -39,26 +39,26 @@ let polygon1 = new Terraformer.Polygon({
   type: "Polygon",
   coordinates: [
     [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
-    [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]],
-  ],
+    [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]
+  ]
 });
 
 let polygon2 = new Terraformer.Polygon([
   [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
-  [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]],
+  [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]
 ]);
 
 let multipolygon1 = new Terraformer.MultiPolygon({
   type: "MultiPolygon",
   coordinates: [[
     [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
-    [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]],
-  ]],
+    [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]
+  ]]
 });
 
 let multipolygon2 = new Terraformer.MultiPolygon([
   [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
-  [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]],
+  [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]
 ]);
 
 let feature1 = new Terraformer.Feature<GeoJSON.Polygon>({
@@ -68,29 +68,29 @@ let feature1 = new Terraformer.Feature<GeoJSON.Polygon>({
     type: "Polygon",
     coordinates: [
       [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
-      [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]],
-    ],
-  },
+      [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]
+    ]
+  }
 });
 
 let feature2 = new Terraformer.Feature({
   type: "Polygon",
   coordinates: [
     [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
-    [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]],
-  ],
+    [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]
+  ]
 });
 
 let featurecollection1 = new Terraformer.FeatureCollection({
   type: "FeatureCollection",
-  features: [feature1, feature2],
+  features: [feature1, feature2]
 });
 
 let featurecollection2 = new Terraformer.FeatureCollection([feature1, feature2]);
 
 let geometrycollection1 = new Terraformer.GeometryCollection({
   type: "GeometryCollection",
-  geometries: [point2, polygon1],
+  geometries: [point2, polygon1]
 });
 
 let geometrycollection2 = new Terraformer.GeometryCollection([point2, polygon1]);
@@ -109,8 +109,8 @@ let polygon = {
     [-112.074279, 40.853293],
     [-111.610107, 40.853293],
     [-111.610107, 40.52215],
-    [-112.074279, 40.52215],
-  ]],
+    [-112.074279, 40.52215]
+  ]]
 };
 
 let polygonGeometry = polygon.coordinates;
